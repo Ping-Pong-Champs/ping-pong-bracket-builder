6dad262ba4ef3a36cb37f1b12325cb77a7fe3c18
tdrobinson94 <tdrobinson94@gmail.com>
2016-07-29 13:37:35 -0400

    messing around with pulling tournaments

--- src/js/controllers/live.tourney.controller.js
+++ src/js/controllers/live.tourney.controller.js
@@ -1,7 +1,19 @@
-function LiveTourneyController() {
+function LiveTourneyController(UserService, TourneyService, $stateParams) {
+
+  let vm = this;
+
+  function init(){
+
+    TourneyService.getTourney($stateParams.id).then( res => {
+        vm.tourney = res.data;
+        console.log(vm.tourney);
+    });
+
+  }
+  init();
 
 }
 
-LiveTourneyController.$inject = [];
+LiveTourneyController.$inject = ['UserService', 'TourneyService', '$stateParams'];
 
 export { LiveTourneyController };
