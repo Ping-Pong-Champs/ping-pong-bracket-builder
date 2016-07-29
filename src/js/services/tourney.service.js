a6fde5db8b374a026d77b635fdfaaa0f6276ad33
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-07-29 15:01:35 -0400

    Created addPlayer function

--- src/js/services/tourney.service.js
+++ src/js/services/tourney.service.js
@@ -3,6 +3,9 @@ function TourneyService ($cookies, $http, SERVER, UserService) {
   this.postTourney = postTourney;
   this.getTourneys  = getTourneys;
   this.getTourney = getTourney;
+  this.createPlayer = createPlayer;
+  this.addPlayerInTourney = addPlayerInTourney;
+  this.getPlayerByNickname = getPlayerByNickname;
 
 
   function postTourney (tourney) {
@@ -20,6 +23,18 @@ function TourneyService ($cookies, $http, SERVER, UserService) {
   function seedTourney(id){
     return $http.post(SERVER.URL + 'tournaments/' + id + '/seed', null, UserService.headers());
   }
+
+  function createPlayer(player){
+  	return $http.post(SERVER.URL + 'PLAYERS', player, UserService.headers());
+  }
+
+  function addPlayerInTourney (player, tourney){
+  	return $http.post(SERVER.URL + 'tournaments/' + tourney.id +'/add-player', player, UserService.headers());
+  }
+
+  function getPlayerByNickname (player){
+		return $http.get(SERVER.URL + 'players/' + player.nickname );
+	}
 }
 
 TourneyService.$inject = ['$cookies', '$http', 'SERVER', 'UserService'];
